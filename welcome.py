476215054db0eab447460a3efa8fee82d7bf9688
thotaveera88@gmail.com <thotaveera88@gmail.com>
2025-04-20 02:00:54 +0000

    Welcome file format changed to .py format

---
+++ welcome.py
@@ -0,0 +1,11 @@
+# Databricks notebook source
+print("welocome to databricks from the Github")
+
+# COMMAND ----------
+
+for i in range(1,10):
+  print(i,end=" ")
+
+# COMMAND ----------
+
+print("welcome to python full course")
